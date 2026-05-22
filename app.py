fbe80cd985d654e5e18967705e9f999efeb3da1d
traphouse998 <traphouse998@users.noreply.github.com>
2026-05-22 23:15:56 +0300

    railway fix: nixpacks + runtime + db init

--- app.py
+++ app.py
@@ -196,7 +196,8 @@ def seed():
     return redirect(url_for("admin_panel"))
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=False)
